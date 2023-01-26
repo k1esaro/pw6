06202e2cf675d004eb0e0f8108fc1c05a2116bb2
Kiesaro <freestylerspb@gmail.com>
2023-01-26 16:59:41 +0300

    first commit

---
+++ Example41/Program.cs
@@ -0,0 +1,23 @@
+﻿void input(int [] array) {
+    for (int i = 0; i< array.Length ; i++) {
+        array[i]=Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void release(int [] array , int result) {
+    for (int i = 0; i < array.Length ; i++) {
+        if(array[i]>0) {
+            result += 1;
+        }
+    }
+    Console.WriteLine(result);
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+int result = 0;
+input(array);
+Console.Write("Колл-во положительных чисел = ");
+release(array , result);
